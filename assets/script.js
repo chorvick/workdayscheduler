7d93fc47b4266df4003a2721bb406ffef0b7f09a
you know who <chorvickprocode@gmail.com>
2021-01-22 15:12:21 -0500

    added document ready and pseudo code to comments

--- assets/script.js
+++ assets/script.js
@@ -35,3 +35,25 @@ for (var i = 0; i < activetime.length; i++) {
 
 
 }
+
+
+///we need 3 fields side by side and the time slots run hourly from 9 am to 5 pm
+/// we need to run the entire thing though a loop that will compare the times
+/// on each row to the current time (will pull current time from moment js and will only need hour to compare since this does not break down further)
+/// events in the past are grey --
+///the current hour needs to be red and future hours green
+/// i will use conditional statements to change the css 
+/// for saving the information we need to be able to write what is in the middle column 
+/// to local storage using JSON when someone clicks a button in the right most column
+// not sure how we will treat multiple entries yet (maybe add on to what is there since
+/// if the page is reloaded the information is supossed to stay )
+//
+// i am forced to use bootstrap by the assignment template--
+// we can add classes and use jquerry to create the template
+//// what i have so far is borrowed in concept from an early exercise we did in class
+/// might be a better way to accomplish it
+/// creation of divs/ spans / asides ect will need to be done in the js file here with 
+/// jquerry 
+// 
+///
+///
