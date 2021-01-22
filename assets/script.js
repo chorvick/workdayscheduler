5772153611c876ba6233cbf6d12024083a7f1455
you know who <chorvickprocode@gmail.com>
2021-01-21 21:18:09 -0500

    jquerry now making divs

--- assets/script.js
+++ assets/script.js
@@ -3,4 +3,35 @@
 
 var momdate = moment();
 momdate = (momdate.format("MMMM Do YYYY"));
-alert(momdate);
+
+var showtime = $("#currentday");
+
+
+var thetime = $("<p>" + momdate + "</p>");
+showtime.append(thetime);
+console.log(thetime);
+//alert(momdate);
+/// 
+var activetime = [
+    "9 AM ",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM"
+];
+var mainDiv = $("#mainarea");
+for (var i = 0; i < activetime.length; i++) {
+
+
+    var newmainDiv = $("<div>" + activetime[i] + "</div>");
+
+    // It then adds this new div to main div.
+    mainDiv.append(newmainDiv);
+    mainDiv.addClass("col-md-9");
+
+
+}
