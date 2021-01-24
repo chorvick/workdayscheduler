3d50f27c98ac5f091a2ada505ebb936b0c2ae6ec
you know who <chorvickprocode@gmail.com>
2021-01-24 00:26:04 -0500

    can capture text from a box - added comments

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,19 @@
 ///js file most action will take place here
+//// make an object called day which will store the text the user inputs 
+var day = {
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+}
+
+
+
 
 $(document).ready(function () {
     var momdate = moment();
@@ -8,41 +23,44 @@ $(document).ready(function () {
     thetime.text(momdate);
 
 
+    //// this code will probably not be used now that we have made the 
+    /// template in the index file --- commented out for now but will
+    /// likely not use  still may be helpful later 
+    // var mainDiv = $("#mainarea");
+    // var ampm = "";
+    // for (var i = 9; i < 18; i++) {
+    //     if (i >= 12) {
+    //         ampm = " PM";
+    //     } else { ampm = " AM" }
 
-    var mainDiv = $("#mainarea");
-    var ampm = "";
-    for (var i = 9; i < 18; i++) {
-        if (i >= 12) {
-            ampm = " PM";
-        } else { ampm = " AM" }
+
+    //     var j = i;
+    //     if (i > 12) {
+    //         j = i - 12
+    //     }
+    //     var newmainDiv = $("<div>" + j + ampm + "</div>");
+    //
+    //mainDiv.append(newmainDiv);
+    //   mainDiv.addClass("row");
+    // mainDiv.addClass("col-md-9");
 
 
-        var j = i;
-        if (i > 12) {
-            j = i - 12
-        }
-        var newmainDiv = $("<div>" + j + ampm + "</div>");
-        //
-        //mainDiv.append(newmainDiv);
-        //   mainDiv.addClass("row");
-        // mainDiv.addClass("col-md-9");
-
-    }
-
+    /// used j querry to put a black border around everything for now to be able to easly 
+    /// see each section
 
     $("div").css("border", "2px solid black");
-    //$("div.first").css("border", "2px solid red");
+
+
+
+    $("#btn9").click(function () {
+        var txt = $("#area9").val();
+        alert(txt);
+    });
+
+
 
 
 });
-    //mainDiv.addClass("input-group mb-3");
-    //  mainDiv.addClass("input-group-prepend");
-
-
-
-    //   $("div").append("<button>");
-
-
 ///we need 3 fields side by side and the time slots run hourly from 9 am to 5 pm
 /// we need to run the entire thing though a loop that will compare the times
 /// on each row to the current time (will pull current time from moment js and will only need hour to compare since this does not break down further)
@@ -64,11 +82,12 @@ $(document).ready(function () {
 
 
 // i am forced to use bootstrap by the assignment template--
-// we can add classes and use jquerry to create the template
-//// 
-/// 
-/// creation of divs/ spans / asides ect will need to be done in the js file here with 
-/// jquerry 
+// on advice from instuctor decided to use html to make the form in index file
+/// then use jquerry to get storage working and worry about the
+///changing of colors last
+///  
+// so will scrap code where the template is made here , 
+// was much easier to make in the index file...
 // 
 ///
 ///
